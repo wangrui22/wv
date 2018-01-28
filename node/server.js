36f37ec2c081a2cb81a77dc269682421cba97bb7
wangrui22 <worry_wang@163.com>
2018-01-29 02:04:39 +0800

    add code

--- node/server.js
+++ node/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const consolidate = require('consolidate');
 const bodyParser = require('body-parser');
-const socketio = require('socket.io');
-let app = express();
-let server = require
-app.listen(8080);
+const net = require('net');
 
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+http.listen(8080);
+
+//web server
 app.engine('html', consolidate.ejs);
 app.set('views', './views');
 app.set('view engine', 'html');
@@ -19,3 +22,33 @@ app.use('/', router);
 router.get('/' , function(req,res) {
     res.render('index'); 
 });
+
+//socket io
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    let client = net.createConnection({host:"127.0.0.1", port:1234}, function() {
+        console.log('tcp connect success.');
+    });
+
+    client.on('data', function(data){
+        console.log('data stream >>>>');
+        socket.emit('data', data);
+    });
+    
+    client.on('end', function(data){
+        console.log('close connection.');
+    });
+
+    socket.on('message', function(json) {
+        console.log(json);
+        //image request
+        if (json.id == 1 ) {
+            let buffer = new Buffer(16);
+            buffer.writeIntLE(1,0,4);
+            client.write(buffer);
+            console.log('client write');
+        }
+    });
+});
+
